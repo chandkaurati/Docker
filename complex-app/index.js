75fa80935bcd335aaa9b55a96c7b7966f88831d7
chandkaurati <chandkaurati@outlook.com>
2024-09-28 20:40:40 +0530

    learned docker netwroks and volumes

---
+++ complex-app/index.js
@@ -0,0 +1,40 @@
+import express from "express";
+import mongoose, { mongo } from "mongoose";
+const app = express();
+const PORT = 3000;
+
+(async () => {
+  try {
+    const connectionInstance = await mongoose.connect(
+      "mongodb://database2:27017"
+    );
+    if (connectionInstance) {
+      console.log("Mongo connecte succefully");
+      console.log(connectionInstance.connection.host);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+})();
+
+const userSchema = new  mongoose.Schema({
+    username : String,
+    // date /: {type: Date, default : new Date.now()}
+})
+
+const User = mongoose.model("User", userSchema)
+app.get("/",  (req, res) => {
+  res.status(200).json({ messsage: "OK" });
+});
+
+app.get("/create",  (req, res) => {
+  
+  const user1 =  User("chand")
+   user1.save()
+  res.status(200).json({ messsage: "OK" });
+  
+});
+
+app.listen(PORT, () => {
+  console.log(`app is listning on port ${PORT}`);
+});
